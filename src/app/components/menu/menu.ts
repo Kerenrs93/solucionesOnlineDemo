2b9e20b0ff151d11cad2aee0b58690f1a27a0dbc
Kerenrs93 <kerenrs93@gmail.com>
2021-05-18 14:28:01 -0600

    crear componente de menu

---
+++ src/app/components/menu/menu.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.html',
+  styleUrls: ['./menu.css']
+})
+export class menuComponent {
+  title = 'solucionesOnlineDemo';
+}
