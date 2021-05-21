78368b7c36683961422bb53abe070b1141a21f20
Kerenrs93 <kerenrs93@gmail.com>
2021-05-20 20:03:39 -0600

    conectar el componente login con el servicio de getuser para enviar los datos al api y validar el usuario

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {FormGroup, FormControl,Validators} from '@angular/forms';
+import {RegisterService} from "../../services/register.service";
+import { Router } from '@angular/router';
 
 @Component({
     selector:'app-login',
@@ -6,4 +9,35 @@ import { Component } from '@angular/core';
     styleUrls:['./login.css']
 })
 
-export class loginComponnet{}
+export class loginComponnet implements OnInit 
+{
+    formLogin: FormGroup;
+    constructor(public registerService:RegisterService, public router:Router) {
+        this.form();
+    }
+
+  ngOnInit() {
+  }
+  private form(){
+      this.formLogin = new FormGroup({
+      nameLogin: new FormControl('', [Validators.required]),
+      passwordLogin: new FormControl('', [Validators.required]),
+    });
+  }
+
+  validateUser(event:Event){
+      event.preventDefault();
+      const user={nameUser:this.formLogin.get('nameLogin').value, passwordUser:this.formLogin.get('passwordLogin').value}
+      console.log(user);
+      this.registerService.getUser(user)
+      .subscribe((User)=>{
+        Object.keys(User).map((key) => {
+        console.log(User[key]);
+            User[key]==true ?  this.router.navigateByUrl('post'):  console.log('no existe');;
+        });
+
+
+    });
+  }
+}
+
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  private api="http://127.0.0.1:8000/api";
 
-  constructor() { }
-  registerUserService(registerUserData){
-    console.log("entro al service");
-    console.log(registerUserData);
+  constructor(private http: HttpClient) { }
+
+
+  getUser(userData){
+    const path=`${this.api}/register`;
+    //const response=this.http.get(path,userData,{responseType: ''});
+    const response=this.http.post(path,userData);
+    return response;
+  }
+
+  createUser(registerUserData){
+    const path=`${this.api}/register`;
+    // let encoded_data = JSON.stringify({registerUserData});
+     console.log(registerUserData);
+    const response=this.http.post(path,registerUserData,{responseType: 'text'});
+    return response;
   }
 }
