dd57a6708fbf95cb686e65b4a44dd6b94f155b9c
Kerenrs93 <kerenrs93@gmail.com>
2021-05-22 22:30:27 -0600

    Eliminar las rutas hijas de posts y agregar la ruta de chat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {loginComponnet} from './components/login/login';
 import {registerComponent} from './components/register/register';
+import {PostComponent} from './components/post/post.component';
+import {ChatComponent} from './components/chat/chat.component'
+
 const routes: Routes = [
+  { path: '', redirectTo: 'post', pathMatch: 'full' },
   { path: 'login', component: loginComponnet },
   { path: 'register', component: registerComponent },
+  { path: 'post/:id', component: PostComponent },
+  { path: 'chat/:id/:idUser/:idpost', component: ChatComponent}
+  
 ];
 
 @NgModule({
