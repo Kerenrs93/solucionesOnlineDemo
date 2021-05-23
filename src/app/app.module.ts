6820b5afc09c6ed4568f5540c5925c9378f3850e
Kerenrs93 <kerenrs93@gmail.com>
2021-05-22 22:29:27 -0600

    agregar componentes en app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+// Components
+import { AppRoutingModule } from './app-routing.module';
+import { menuComponent } from './components/menu/menu';
+import {loginComponnet} from './components/login/login';
+import {registerComponent} from './components/register/register';
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/post/post.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { ChatMessagesComponent } from './components/chat-messages/chat-messages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    menuComponent,
+    loginComponnet,
+    registerComponent,
+    PostComponent,
+    ChatComponent,
+    ChatMessagesComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
